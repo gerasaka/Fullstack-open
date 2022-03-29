9e28dc5a240de0e93107c2398f5a7276ab9f585a
gerasaka <gerryjulioo@gmail.com>
2022-03-29 18:05:25 +0700

    feat: finished exercise 1.1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,3 +1,15 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Content = (props) => (
+  <p>
+    {props.part} {props.exercises}
+  </p>
+);
+
+const Total = ({ num1, num2, num3 }) => (
+  <p>Number of exercises {num1 + num2 + num3}</p>
+);
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -9,17 +21,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total num1={exercises1} num2={exercises2} num3={exercises3} />
     </div>
   );
 };
