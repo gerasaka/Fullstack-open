34fdc9c6041f6b6b28f64d7f6218097164fefb9d
gerasaka <gerryjulioo@gmail.com>
2022-10-09 20:31:32 +0700

    feat: finished exercise 2.4 & 2.5

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -4,12 +4,12 @@ import { Content } from './Content';
 import { Header } from './Header';
 import { Total } from './Total';
 
-export const Course = ({ course }) => {
-  return (
-    <>
+export const Course = ({ courses }) => {
+  return courses.map(course => (
+    <div key={course.id}>
       <Header courseName={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
-    </>
-  );
+    </div>
+  ));
 };
